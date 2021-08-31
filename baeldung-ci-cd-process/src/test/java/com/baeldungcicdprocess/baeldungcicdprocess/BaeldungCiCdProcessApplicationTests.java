94549336fb9926ed699fb5dd4a425bf8891115c9
Mohammed <edris.mohammed@steampunk.com>
2021-08-30 20:17:08 -0400

    Initialize application

---
+++ baeldung-ci-cd-process/src/test/java/com/baeldungcicdprocess/baeldungcicdprocess/BaeldungCiCdProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldungcicdprocess.baeldungcicdprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeldungCiCdProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
